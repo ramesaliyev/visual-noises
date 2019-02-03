7e722c4ee4c7b9a1781fbffc7a30591d739ab18a
Rameş Aliyev <creator@ramesaliyev.com>
2019-02-04 00:48:43 +0300

    :zap: use web worker for colorful 2d image

--- visualisations/2d-colorful-image.js
+++ visualisations/2d-colorful-image.js
@@ -8,16 +8,33 @@ function draw2DColorfulImage({
   height -= 100;
   width -= 200;
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const r = map(0, 1, 0, 255, getValueFn(x/amplitude, y/amplitude)[0] / amplitude);
-      const g = map(0, 1, 0, 255, getValueFn(x/amplitude, y/amplitude)[1] / amplitude);
-      const b = map(0, 1, 0, 255, getValueFn(x/amplitude, y/amplitude)[2] / amplitude);
+  text(offsetX/2 + screenWidth/2, screenHeight/2, 'Calculating...', {color: '#666'});
 
-      const xPos = offsetX + x + 100;
-      const yPos = offsetY + height + 100 - y;
+  const calculate = () => {
+    const result = [];
 
-      line(xPos, yPos, xPos, yPos - 1, {color: `rgb(${r}, ${g}, ${b})`});
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const r = map(0, 1, 0, 255, getValueFn(x/amplitude, y/amplitude)[0] / amplitude);
+        const g = map(0, 1, 0, 255, getValueFn(x/amplitude, y/amplitude)[1] / amplitude);
+        const b = map(0, 1, 0, 255, getValueFn(x/amplitude, y/amplitude)[2] / amplitude);
+
+        const xPos = offsetX + x + 100;
+        const yPos = offsetY + height + 100 - y;
+
+        result.push({xPos, yPos, r, g, b});
+      }
     }
+
+    return result;
   }
+
+  run(calculate, {getValueFn, height, width, offsetX, offsetY}, result => {
+    context.clearRect(0, 0, screenWidth, screenHeight);
+
+    result.forEach(({xPos, yPos, r, g, b}) => {
+      context.fillStyle = `rgb(${r}, ${g}, ${b})`;
+      context.fillRect(xPos, yPos, 1, 1);
+    });
+  });
 }
